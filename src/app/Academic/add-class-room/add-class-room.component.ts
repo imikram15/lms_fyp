5e4c8db4f26d0bdb967cdc814fd7450692eca925
Syed Ali Murtaza shah <murtazasyedali098@gmail.com>
2024-03-24 16:29:04 +0500

    filter button

---
+++ src/app/Academic/add-class-room/add-class-room.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-class-room',
+  templateUrl: './add-class-room.component.html',
+  styleUrl: './add-class-room.component.scss'
+})
+export class AddClassRoomComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+classForm: any;
+
+}
