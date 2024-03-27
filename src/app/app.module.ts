5e8797102541a75716a331a4b5f2204e5ae60505
DESKTOP-R01I010\Ikram <buttikram15@gmail.com>
2024-03-27 17:09:27 +0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
